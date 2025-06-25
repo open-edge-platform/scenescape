7395f283a560fa1f1ebb269bbbf86ba0843178b0
Iracki, Patryk <patryk.iracki@intel.com>
2025-06-25 10:45:23 +0000

    first fix only files without copyright and license

--- add_missing_license.py
+++ add_missing_license.py
@@ -15,5 +15,6 @@ with open(json_file, 'r') as f:
 pprint.pp(data)
 
 for item in data.get("non_compliant").get("missing_licensing_info", []):
-    print(f"Adding license to {item}")
-    subprocess.run(["make", "add-licensing", "FILE=" + item])
+    if item in data.get("non_compliant").get("missing_copyright_info", []):
+        print(f"Adding license to {item}")
+        subprocess.run(["make", "add-licensing", "FILE=" + item])
