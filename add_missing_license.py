6314d7f07c2f302519d6ba39b38bbdf014c1aa93
Iracki, Patryk <patryk.iracki@intel.com>
2025-06-25 11:17:19 +0000

    update script to replace old licensing format with the new one

--- add_missing_license.py
+++ add_missing_license.py
@@ -5,9 +5,10 @@
 import json
 import subprocess
 import pprint
+import fileinput
+import re
 
 json_file = 'reuse_lint.json'  # Change this to your JSON file path
-
 
 with open(json_file, 'r') as f:
     data = json.load(f)
@@ -17,4 +18,11 @@ pprint.pp(data)
 for item in data.get("non_compliant").get("missing_licensing_info", []):
     if item in data.get("non_compliant").get("missing_copyright_info", []):
         print(f"Adding license to {item}")
+        subprocess.run(["make", "add-licensing", "FILE=" + item])
+    else:
+        for line in fileinput.input(item, inplace=True):
+            if re.match(r'.*Copyright \(C\).*',line):
+                print('{}'.format(line.replace("Copyright (C)","SPDX-FileCopyrightText: (C)")),end='') # for Python 3
+            else:
+                print('{}'.format(line),end='')
         subprocess.run(["make", "add-licensing", "FILE=" + item])
