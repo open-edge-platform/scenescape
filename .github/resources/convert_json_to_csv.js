898cbb65029c9c7bd04c24c90866942cdf0fb926
Yermolenko, Dmytro <dmytro.yermolenko@intel.com>
2025-07-09 22:49:05 +0200

    Trivy .csv output test 7

--- .github/resources/convert_json_to_csv.js
+++ .github/resources/convert_json_to_csv.js
@@ -5,19 +5,23 @@ const convertJsonToCsv = (jsonFilePath, csvFilePath) => {
   const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8'));
 
   const fields = ['Target', 'VulnerabilityID', 'Severity', 'CVSS Score', 'Title', 'Library', 'Vulnerable Version', 'Fixed Version', 'Information URL'];
-  const csvData = jsonData.Results.flatMap(result => 
-    result.Vulnerabilities.map(vulnerability => ({
+  const csvData = jsonData.Results.flatMap(result => {
+    if (!result.Vulnerabilities) {
+      console.warn(`No vulnerabilities found for target: ${result.Target}`);
+      return [];
+    }
+    return result.Vulnerabilities.map(vulnerability => ({
       Target: result.Target,
-      VulnerabilityID: vulnerability.VulnerabilityID,
-      Severity: vulnerability.Severity,
+      VulnerabilityID: vulnerability.VulnerabilityID || '',
+      Severity: vulnerability.Severity || '',
       'CVSS Score': vulnerability.CVSS?.nvd?.V3Score || '',
-      Title: vulnerability.Title,
-      Library: vulnerability.PkgName,
-      'Vulnerable Version': vulnerability.InstalledVersion,
-      'Fixed Version': vulnerability.FixedVersion,
-      'Information URL': vulnerability.PrimaryURL
-    }))
-  );
+      Title: vulnerability.Title || '',
+      Library: vulnerability.PkgName || '',
+      'Vulnerable Version': vulnerability.InstalledVersion || '',
+      'Fixed Version': vulnerability.FixedVersion || '',
+      'Information URL': vulnerability.PrimaryURL || ''
+    }));
+  });
 
   const csv = parse(csvData, { fields });
   fs.writeFileSync(csvFilePath, csv);
