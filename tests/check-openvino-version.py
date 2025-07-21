3194b6034f64e674f1751fd66ffefa09c510572c
Iracki, Patryk <patryk.iracki@intel.com>
2025-07-21 11:43:15 +0000

    Revert "ignore bandit errors in test code"
    This reverts commit e509d9e150604faff490a26ccd9dd7a50f914f74.

--- tests/check-openvino-version.py
+++ tests/check-openvino-version.py
@@ -17,7 +17,7 @@ def main():
   exit_code = 1
   try:
     package_name = "openvino"
-    proc = subprocess.Popen(["pip index versions " + package_name], stdout=subprocess.PIPE, shell=True) # nosec B602
+    proc = subprocess.Popen(["pip index versions " + package_name], stdout=subprocess.PIPE, shell=True)
     (proc_output, err) = proc.communicate()
 
     proc_output = proc_output.decode()
