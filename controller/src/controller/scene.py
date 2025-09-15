74576e8d4baa8bd3b870ac0282703f707eb39f23
Yermolenko, Dmytro <dmytro.yermolenko@intel.com>
2025-09-15 12:44:37 +0200

    WIP

--- controller/src/controller/scene.py
+++ controller/src/controller/scene.py
@@ -17,6 +17,7 @@ from scene_common.transform import CameraPose
 from scene_common.mesh_util import getMeshAxisAlignedProjectionToXY, createRegionMesh, createObjectMesh
 
 from controller.ilabs_tracking import IntelLabsTracking
+from copy import deepcopy
 from controller.tracking import (MAX_UNRELIABLE_TIME,
                                  NON_MEASUREMENT_TIME_DYNAMIC,
                                  NON_MEASUREMENT_TIME_STATIC)
@@ -229,8 +230,14 @@ class Scene(SceneModel):
           for pair in category_pairs
         ])
         log.info("Average position for category %s: (%.2f, %.2f)" % (category, av_x, av_y))
-      # clustered.append(objects[0])
-      # objects[:] = clustered
+        # Create a new object as a copy of the first object in the first close pair
+        first_pair = category_pairs[0]
+        new_obj = deepcopy(objects[first_pair[0]])
+        # Modify its coordinates to the average position
+        new_obj.sceneLoc.x = av_x
+        new_obj.sceneLoc.y = av_y
+        clustered.append(new_obj)
+      objects[:] = clustered
     return
 
   def _finishProcessing(self, detectionType, when, objects, already_tracked_objects=[]):
