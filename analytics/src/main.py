6bcfe575e14d3b5418de427f5d026313343bb927
Yermolenko, Dmytro <dmytro.yermolenko@intel.com>
2025-09-24 13:55:54 +0200

    Add paho-mqtt==2.1.0 to requirements

--- analytics/src/main.py
+++ analytics/src/main.py
@@ -2,6 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 #!/usr/bin/env python3
+
+from scene_common.mqtt import PubSub
+
 def main():
     print("Hello from Docker container!")
     print("Python version:")
@@ -9,6 +12,8 @@ def main():
     print(sys.version)
 
     import datetime
+    def loopForever(self):
+        return self.pubsub.loopForever()
     while True:
         # Import and use other modules from src
         try:
