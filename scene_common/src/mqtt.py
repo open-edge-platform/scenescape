15a7444dd06aff6a43c5b5fb7e287fa6b5297bea
Yermolenko, Dmytro <dmytro.yermolenko@intel.com>
2025-09-25 17:45:03 +0200

    remove sys from topic path in mqtt

--- scene_common/src/mqtt.py
+++ scene_common/src/mqtt.py
@@ -66,7 +66,7 @@ class PubSub(_PubSubTopicBase):
     _Topic.IMAGE_CAMERA: Template(TOPIC_BASE + "/image/camera/${camera_id}"),
     _Topic.SYS_AUTOCALIB_STATUS: Template(TOPIC_BASE + "/sys/autocalibration/status"),
     _Topic.SYS_CHILDSCENE_STATUS: Template(TOPIC_BASE + "/sys/child/status/${scene_id}"),
-    _Topic.ANALYTICS_CLUSTERS: Template(TOPIC_BASE + "/sys/analytics/clusters/${scene_id}"),
+    _Topic.ANALYTICS_CLUSTERS: Template(TOPIC_BASE + "/analytics/clusters/${scene_id}"),
   }
 
   def __init__(self, auth, cert, rootca, broker, port=None, keepalive=60,
