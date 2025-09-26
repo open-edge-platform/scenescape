bdc441c13c80de87d3551b89a3af6843d7015892
Tomasz Dorau <tomasz.dorau@intel.com>
2025-09-26 16:16:28 +0200

    Add msg in script

--- run-perf-test.sh
+++ run-perf-test.sh
@@ -4,5 +4,5 @@ set -e
 
 mkdir -p logs
 cp docker-compose-percebro-$1.yml docker-compose.yml
+echo "Logging to logs/run-percebro-perf-test-$1.log"
 docker compose up > logs/run-percebro-perf-test-$1.log 2>&1
-
