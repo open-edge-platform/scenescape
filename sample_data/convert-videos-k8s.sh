c353cf6374e1c2f24e9e99d12715ee2806e83d97
Iracki, Patryk <patryk.iracki@intel.com>
2025-10-27 11:55:43 +0100

    Addressed review comments

---
+++ sample_data/convert-videos-k8s.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+echo "Converting .mp4 files to .ts..."
+if [ -f /workspace/sample-data-storage/sample_data/.done ]; then
+  echo ".done file exists in /workspace/sample-data-storage/sample_data/"
+else
+  for mp4_file in /workspace/sample-data-storage/sample_data/*.mp4; do
+      ts_file="${mp4_file%.mp4}.ts"
+      echo "Converting $mp4_file to $ts_file..."
+      /ffmpegwrapper.sh -i "$mp4_file" -c copy "$ts_file"
+  done
+fi
