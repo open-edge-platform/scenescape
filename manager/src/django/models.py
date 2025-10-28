dae60fa0647803714d48e0077b74df62a048582a
Iracki, Patryk <patryk.iracki@intel.com>
2025-10-28 12:42:40 +0100

    added camcalibration URL as ENV:

--- manager/src/django/models.py
+++ manager/src/django/models.py
@@ -44,6 +44,7 @@ from scene_common import log
 def sendUpdateCommand(scene_id=None, camera_data=None):
   broker = os.environ.get("BROKER")
   auth = os.environ.get("BROKERAUTH")
+  camcalibration_url = os.environ.get("CAMCALIBRATION_URL")
   rootcert = os.environ.get("BROKERROOTCERT")
   if rootcert is None:
     rootcert = "/run/secrets/certs/scenescape-ca.pem"
@@ -57,7 +58,7 @@ def sendUpdateCommand(scene_id=None, camera_data=None):
     else:
       if scene_id:
         client.publish(PubSub.formatTopic(PubSub.CMD_SCENE_UPDATE, scene_id = scene_id), "update")
-        url = f"https://camcalibration.scenescape.intel.com:8443/v1/scenes/{scene_id}/registration"
+        url = f"https://{camcalibration_url}/v1/scenes/{scene_id}/registration"
         headers = {
           "Content-Type": "application/json"
         }
