2b67b50e9b378832cb74069b98c40513b4023bc5
Derrick Addo <derrick.addo@intel.com>
2025-11-03 09:54:58 -0800

    Fix map & polycam_data path issue & group ID issues

--- autocalibration/src/auto_camera_calibration_model.py
+++ autocalibration/src/auto_camera_calibration_model.py
@@ -48,7 +48,7 @@ class CalibrationScene:
   @classmethod
   def deserialize(cls, data):
     scene = cls(data['uid'], data['name'])
-    scene.map = "/workspace/media/" + data['map'].split('/')[-1] if 'map' in data else scene.DEFAULTS['map']
+    scene.map = "/home/scenescape/SceneScape/media/" + data['map'].split('/')[-1] if 'map' in data else scene.DEFAULTS['map']
     scene.scale = data.get('scale', scene.DEFAULTS['scale'])
     scene.mesh_rotation = data.get('mesh_rotation', scene.mesh_rotation)
     scene.mesh_translation = data.get('mesh_translation', scene.mesh_translation)
@@ -56,7 +56,7 @@ class CalibrationScene:
     scene.apriltag_size = data.get('apriltag_size', scene.DEFAULTS['apriltag_size'])
     scene.map_processed = get_datetime_from_string(data['map_processed']) if 'map_processed' in data else scene.DEFAULTS['map_processed']
     scene.camera_calibration = data.get('camera_calibration', scene.DEFAULTS['camera_calibration'])
-    scene.polycam_data = "/workspace/media/" + data['polycam_data'].split('/')[-1] if 'polycam_data' in data else scene.DEFAULTS['polycam_data']
+    scene.polycam_data = "/home/scenescape/SceneScape/media/" + data['polycam_data'].split('/')[-1] if 'polycam_data' in data else scene.DEFAULTS['polycam_data']
     scene.number_of_localizations = data.get('number_of_localizations', scene.DEFAULTS['number_of_localizations'])
     scene.global_feature = data.get('global_feature', scene.DEFAULTS['global_feature'])
     scene.local_feature = data.get('local_feature', scene.DEFAULTS['local_feature'])
