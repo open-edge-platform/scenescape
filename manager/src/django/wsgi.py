edcfe22d78682bb58a133b4fe8a48f8a0ae6910e
Kasprzak, Mikolaj <mikolaj.kasprzak@intel.com>
2025-11-05 14:57:32 +0100

    fix indentation

--- manager/src/django/wsgi.py
+++ manager/src/django/wsgi.py
@@ -20,6 +20,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manager.settings")
 # Add the project directory to Python path if not already there
 project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_dir not in sys.path:
-    sys.path.insert(0, project_dir)
+  sys.path.insert(0, project_dir)
 
 application = get_wsgi_application()
