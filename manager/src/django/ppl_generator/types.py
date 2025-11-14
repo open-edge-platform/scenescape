48d600b41ba85df73e5c7150be986032a04a812b
Dorau, Tomasz <tomasz.dorau@intel.com>
2025-11-14 11:06:25 +0100

    Refactor ppl_generator into a Python module

---
+++ manager/src/django/ppl_generator/types.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from enum import IntEnum
+
+
+class PipelineGenerationNotImplementedError(NotImplementedError):
+  pass
+
+
+class PipelineGenerationValueError(ValueError):
+  pass
+
+
+class InferenceRegion(IntEnum):
+  FULL_FRAME = 0
+  ROI_LIST = 1
