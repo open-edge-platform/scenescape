d6b67c5fbc3d3fddc69b9e4b676d9ce59b865c41
Kasprzak, Mikolaj <mikolaj.kasprzak@intel.com>
2025-11-25 15:49:14 +0100

    fix intendation

--- manager/src/django/scene_import.py
+++ manager/src/django/scene_import.py
@@ -61,17 +61,17 @@ class ImportScene:
     }
 
     json_files = [
-        f for f in os.listdir(self.extract_dir)
-        if os.path.isfile(os.path.join(self.extract_dir, f)) and f.lower().endswith(".json")
+      f for f in os.listdir(self.extract_dir)
+      if os.path.isfile(os.path.join(self.extract_dir, f)) and f.lower().endswith(".json")
     ]
 
     if not json_files:
-        errors["scene"] = {"scene": ["No JSON file found"]}
-        return errors
+      errors["scene"] = {"scene": ["No JSON file found"]}
+      return errors
 
     if len(json_files) > 1:
-        errors["scene"] = {"scene": ["Multiple JSON files found"]}
-        return errors
+      errors["scene"] = {"scene": ["Multiple JSON files found"]}
+      return errors
 
     json_file = os.path.join(self.extract_dir, json_files[0])
 
