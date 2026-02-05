c687b2b5e14809cb9f2d492c93b0ba80402afd67
Józef Daniecki <jozef.daniecki@intel.com>
2026-02-05 15:12:14 +0100

    fix service test

--- tracker/inc/message_handler.hpp
+++ tracker/inc/message_handler.hpp
@@ -66,7 +66,7 @@ public:
     static constexpr const char* TOPIC_CAMERA_PREFIX = "scenescape/data/camera/";
 
     /// Topic pattern for camera subscriptions (format with camera_id)
-    static constexpr const char* TOPIC_CAMERA_SUBSCRIBE_PATTERN = "scenescape/data/camera/{}/";
+    static constexpr const char* TOPIC_CAMERA_SUBSCRIBE_PATTERN = "scenescape/data/camera/{}";
 
     /// Topic pattern for scene output (format with scene_id and thing_type)
     static constexpr const char* TOPIC_SCENE_DATA_PATTERN = "scenescape/data/scene/{}/{}";
