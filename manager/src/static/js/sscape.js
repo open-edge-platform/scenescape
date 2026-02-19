035c3a174b675b4d0b42926acc089ce7ed46a38c
Derrick Addo <derrick.addo@intel.com>
2026-02-19 15:17:51 -0800

    pad short images to 518 height

--- manager/src/static/js/sscape.js
+++ manager/src/static/js/sscape.js
@@ -1542,7 +1542,7 @@ function setupGenerateMesh() {
 }
 
 async function pollMeshStatus(sceneId, requestId) {
-  const timeout = 10 * 60 * 1000; // 10 minutes
+  const timeout = 10 * 60 * 1500; // 15 minutes
   const start = Date.now();
 
   while (true) {
