1bbf2f5a2d9695f64e89a99fd1cf621468763ea8
Filip Adamus <50263202+FilipAdamus97@users.noreply.github.com>
2026-03-24 15:43:19 +0100

    Revert log.py changes

--- scene_common/src/scene_common/log.py
+++ scene_common/src/scene_common/log.py
@@ -1,8 +1,7 @@
-# SPDX-FileCopyrightText: (C) 2021 - 2026 Intel Corporation
+# SPDX-FileCopyrightText: (C) 2021 - 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-import logging, sys, os
-from datetime import datetime
+import logging, sys
 
 # Disable logging by default
 LVL_TRACK = 0
@@ -17,58 +16,15 @@ LVL_MIN = 99
 # log.LVL_TRACK = 20
 # log.LVL_MIN = log.LVL_INFO
 
-def _init_loger():
-  """Initialize logger if not already created"""
-  if hasattr(log, "logger"):
-    return
-
-  log.logger = logging.getLogger(__name__)
-  log.logger.setLevel(LVL_DEBUG)
-
-  if not log.logger.handlers:
-    formatter = logging.Formatter(
-      "%(asctime)s [%(levelname)s] %(message)s",
-      datefmt="%Y-%m-%d_%H:%M:%S"
-    )
-
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
-    log.logger.addHandler(console_handler)
-
-def enable_file_logging(test_name):
-  """OPTIONAL: enable file logging for the test if not enabled elsewhere."""
-
-  _init_loger()
-
-  # Prevent adding multiple file handlers
-  if hasattr(log, "filehandler"):
-    return
-
-  log_dir = os.path.join(os.getcwd(), "test_data", "logs")
-  os.makedirs(log_dir, exist_ok=True)
-
-  timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-  log_file = os.path.join(log_dir, f"{test_name}_{timestamp}.log")
-
-  formatter = logging.Formatter(
-      "%(asctime)s [%(levelname)s] %(message)s",
-      datefmt="%Y-%m-%d_%H:%M:%S"
-    )
-
-  file_handler = logging.FileHandler(log_file, mode="w")
-  file_handler.setLevel(LVL_DEBUG)
-  file_handler.setFormatter(formatter)
-
-  log.logger.addHandler(file_handler)
-  log.file_handler = file_handler
-
-  log.logger.info(f"File logging enabled: {log_file}")
-
-
 def log(*args, level=logging.INFO):
-  _init_loger()
-
+  if not hasattr(log, "logger"):
+    log.logger = logging.getLogger(__name__)
+    log.logger.setLevel(level)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter("%(asctime)s %(message)s",
+                                           datefmt="%Y-%m-%d %H:%M:%S"))
+    # handler.setFormatter(logging.Formatter("%(message)s"))
+    log.logger.addHandler(handler)
   outstr = " ".join(map(str, args))
   log.logger.log(level, outstr)
   return
