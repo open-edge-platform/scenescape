11fdb96b6a5c9debbb095f9d86751afccb4d84b0
Filip Adamus <50263202+FilipAdamus97@users.noreply.github.com>
2026-03-25 11:03:00 +0100

    Prepare basic structure and remove file from wrong folder

--- tests/security/system/test_secrets_files_permissions.py
+++ tests/security/system/test_secrets_files_permissions.py
@@ -6,6 +6,7 @@ import re
 from datetime import datetime
 from scene_common import log
 from tests.common_test_utils import record_test_result
+from tests.functional.test_file_permissions import run_find
 
 def get_container_name(pattern, log):
   """Returns the name of a container with specific pattern in name"""
@@ -22,18 +23,28 @@ def get_container_name(pattern, log):
   log.info(f"Container {pattern} not found in the container list.")
   return None
 
+def run_find(container, query):
+  cmd = ["docker", "exec", "-i", container,
+          "-t", "-A", "-c", query]
 
-def test_timestamp_format():
-  """ Verifies that all timestamps are utilizing ISO 8601 UTC format.
+  result = subprocess.run(cmd, capture_output=True, text=True)
+  return result.stdout.strip()
+
+def test_secrets_file_permissions():
+  """ Verifies that all secrets files have expected levels of permission.
 
   Steps:
     * Get web container name
-    *
+    * Run find command to obtain file data
+    * compare to the expected pattern
   """
   test_name = "NEX-T10548"
   exit_code = 1
   log.info(f"Test: {test_name}")
   try:
     web_container = get_container_name("web")
+    cmd = ["find", "run/secrets/", "-type", "f", "|", "xargs", "ls", "-la"]
+    response = run_find(web_container, cmd)
+    exit_code = 0
   finally:
     record_test_result(test_name, exit_code)
