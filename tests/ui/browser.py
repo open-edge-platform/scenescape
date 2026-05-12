10a4162ebda3b45d0fbd738a03e9507c8c4b6c39
Iracki, Patryk <patryk.iracki@intel.com>
2026-05-12 12:47:39 +0200

    fix browser

--- tests/ui/browser.py
+++ tests/ui/browser.py
@@ -36,7 +36,7 @@ class Browser(Firefox):
     options.set_preference("gfx.webrender.software", True)
     options.set_preference("network.proxy.type", 0)
 
-    firefox_path = shutil.which("firefox") or shutil.which("firefox-esr")
+    firefox_path = shutil.which("firefox") or shutil.which("firefox-esr") or "firefox"
     if firefox_path:
       options.binary_location = firefox_path
 
@@ -44,3 +44,87 @@ class Browser(Firefox):
     service = Service(geckodriver_path)
 
     super().__init__(options=options, service=service)
+
+  def getPage(self, url, expected_title, retries=MAX_RETRIES, delay=RETRY_DELAY):
+    '''
+    Will load the page at <url> and check to see if the title
+    matches. Returns True/False.
+    '''
+
+    print("Fetching page")
+    retry = 0
+    success = False
+    while True:
+      try:
+        self.get(url)
+        print(self.title)
+        if self.title == expected_title:
+          success = True
+          break
+      except WebDriverException as e:
+        print("Fetch error")
+
+      retry += 1
+      if retry >= retries:
+        print(f"Failed to get page from server after {retry} tries")
+        break
+      time.sleep(delay)
+
+    return success
+
+  def login(self, user, password, weburl, retries=MAX_RETRIES, delay=RETRY_DELAY):
+    '''
+    Tries to log in using the provided user & password. Returns
+    True/False. If unable to find form fields or error message,
+    raises an exception.
+    '''
+    success = False
+    retry = 0
+    while True:
+      try:
+        self.get(weburl)
+      except WebDriverException as e:
+        print("Fetch error")
+      else:
+        try:
+          field = self.find_element(By.ID, "username")
+        except NoSuchElementException:
+          pass
+        else:
+          field.clear()
+          field.send_keys(user)
+          field = self.find_element(By.ID, "password")
+          field.clear()
+          field.send_keys(password)
+
+          button = self.find_element(By.CSS_SELECTOR, "button.btn-primary")
+          button.click()
+
+          try:
+            self.find_element(By.CSS_SELECTOR, "ul.navbar-nav")
+            success = True
+            break
+          except NoSuchElementException:
+            try:
+              self.find_element(By.CSS_SELECTOR, "ul.errorlist")
+              print("Invalid user/password")
+            except NoSuchElementException:
+              print("Couldn't find login status")
+
+      retry += 1
+      if retry >= retries:
+        print("Failed to login after", retry, "tries")
+        break
+      time.sleep(delay)
+
+    return success
+
+  def setViewportSize(self, width, height):
+    window_size = self.execute_script("""
+        return [window.outerWidth - window.innerWidth + arguments[0],
+          window.outerHeight - window.innerHeight + arguments[1]];
+        """, width, height)
+    return self.set_window_size(*window_size)
+
+  def actionChains(self):
+    return ActionChains(self)
