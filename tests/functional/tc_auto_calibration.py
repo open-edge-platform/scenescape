a9538f9ec830b57f525b3ccb4604b4d9606df6f2
Belhaik, Sadek <sadek.belhaik@intel.com>
2026-05-12 11:57:14 +0200

    remove leftover

--- tests/functional/tc_auto_calibration.py
+++ tests/functional/tc_auto_calibration.py
@@ -292,11 +292,3 @@ def test_auto_calibration(request, record_xml_attribute,
              n_tags, random_select, expect_status,
              expected_result, intrinsics=intrinsics)
   test.runAutoCalibration()
-  assert test.exitCode == 0
-  return test.exitCode
-
-def main():
-  return test_auto_calibration(None, None)
-
-if __name__ == "__main__":
-  os._exit(main() or 0)
