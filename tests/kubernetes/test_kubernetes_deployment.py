d2a5a4f10163787f67921d0117d2b44f28f9f9e6
Iracki, Patryk <patryk.iracki@intel.com>
2026-05-21 17:44:54 +0200

    fix kind

--- tests/kubernetes/test_kubernetes_deployment.py
+++ tests/kubernetes/test_kubernetes_deployment.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-# SPDX-FileCopyrightText: (C) 2023 - 2026 Intel Corporation
+# SPDX-FileCopyrightText: (C) 2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 """
