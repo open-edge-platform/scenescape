a2de553714d8567631e8d6ec1e96661403b0f72c
Iracki, Patryk <patryk.iracki@intel.com>
2026-05-26 12:58:35 +0200

    reenable other images

--- tests/non_functional/test_build_time_and_size.py
+++ tests/non_functional/test_build_time_and_size.py
@@ -28,10 +28,10 @@ class ImageBuildRequirements:
     self.size_limit_megabytes = size_limit_megabytes
 
 IMAGES_REQUIREMENTS = [
-  # ImageBuildRequirements(name="common-base", make_target="build-common", time_limit_seconds=120, size_limit_megabytes=400.0),
-  # ImageBuildRequirements(name="manager", make_target="manager", time_limit_seconds=300, size_limit_megabytes=600.0),
-  # ImageBuildRequirements(name="controller", make_target="controller", time_limit_seconds=240, size_limit_megabytes=600.0),
-  # ImageBuildRequirements(name="autocalibration", make_target="autocalibration", time_limit_seconds=300, size_limit_megabytes=800.0),
+  ImageBuildRequirements(name="common-base", make_target="build-common", time_limit_seconds=120, size_limit_megabytes=400.0),
+  ImageBuildRequirements(name="manager", make_target="manager", time_limit_seconds=300, size_limit_megabytes=600.0),
+  ImageBuildRequirements(name="controller", make_target="controller", time_limit_seconds=240, size_limit_megabytes=600.0),
+  ImageBuildRequirements(name="autocalibration", make_target="autocalibration", time_limit_seconds=300, size_limit_megabytes=800.0),
   ImageBuildRequirements(name="tracker", make_target="tracker", time_limit_seconds=900, size_limit_megabytes=40.0),
 ]
 
