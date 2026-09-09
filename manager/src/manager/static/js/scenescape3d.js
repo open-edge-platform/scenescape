2cebce80653a71db304332002d81f9c032f8f33d
Lukasiuk, Emilia Anna <emilia.anna.lukasiuk@intel.com>
2026-09-09 15:05:38 +0200

    feat: show live roi occupancy in the 3d view

--- manager/src/manager/static/js/scenescape3d.js
+++ manager/src/manager/static/js/scenescape3d.js
@@ -323,6 +323,14 @@ function main() {
     }
   }
 
+  // Stale counts silently misinform once events stop arriving; clear them on disconnect.
+  function clearRegionCountLabels() {
+    const regionThings = sceneThingManagers.things.region.obj.sceneThings;
+    for (const uid in regionThings) {
+      regionThings[uid].removeCountLabel();
+    }
+  }
+
   // MQTT Client
   async function connectMQTT() {
     // MQTT management (see https://github.com/mqttjs/MQTT.js)
@@ -370,15 +378,12 @@ function main() {
           }
         }
 
-        if (sceneThing.isParent) {
-          console.log(
-            "Subscribed to " +
-              (appName + CONSTANTS.EVENT + "/+" + "/" + sceneName + "/+/+"),
-          );
-          client.subscribe(
-            appName + CONSTANTS.EVENT + "/+" + "/" + sceneName + "/+/+",
-          );
-        }
+        console.log(
+          "Subscribed to " + (appName + CONSTANTS.EVENT + "/+/" + sceneID + "/+/+"),
+        );
+        client.subscribe(
+          appName + CONSTANTS.EVENT + "/+/" + sceneID + "/+/+",
+        );
         cameraManager = sceneThingManagers["things"]["camera"]["obj"];
         for (const key in cameraManager.sceneCameras) {
           if (key !== "undefined") {
@@ -396,6 +401,7 @@ function main() {
 
     client.on("error", (e) => {
       console.log("MQTT error: " + e);
+      clearRegionCountLabels();
     });
 
     assetManager = AssetManager(
@@ -578,8 +584,12 @@ function main() {
 
       const analyticsParams = sceneThingManagers.things[analyticsName];
       const currentThings = analyticsParams.obj.sceneThings;
+      const thingKey =
+        msg["metadata"]["uuid"] in currentThings
+          ? msg["metadata"]["uuid"]
+          : childData["name"];
 
-      if (childData["name"] in currentThings) {
+      if (thingKey in currentThings) {
         const analyticsClass = analyticsParams.obj.thingObjects();
 
         const tempChildData = new analyticsClass[analyticsName](childData);
@@ -589,13 +599,17 @@ function main() {
 
         if (
           JSON.stringify(tempChildData.points) !==
-          JSON.stringify(currentThings[childData["name"]].points)
+          JSON.stringify(currentThings[thingKey].points)
         ) {
-          currentThings[childData["name"]].updateShape(childData);
+          currentThings[thingKey].updateShape(childData);
         }
       } else {
         analyticsParams.obj.add(childData);
         analyticsParams.obj.update(0, analyticsParams);
+      }
+
+      if (analyticsName === "region" && msg["counts"]) {
+        currentThings[thingKey]?.updateCounts(msg["counts"]);
       }
     }
   }
