6081fe939af5257953020e6a46bec9e258201ba8
Tomasz Dorau <tomasz.dorau@intel.com>
2026-09-22 13:23:49 +0200

    Use .venv name covered by gitignore file

--- controller/src/robot_vision/benchmarks/compare_benchmarks.sh
+++ controller/src/robot_vision/benchmarks/compare_benchmarks.sh
@@ -31,7 +31,7 @@ fi
 
 
 # Use a Python virtual environment in the current folder, creating it if absent
-VENV_DIR="${SCRIPT_DIR}/venv"
+VENV_DIR="${SCRIPT_DIR}/.venv"
 if [ ! -f "$VENV_DIR/bin/activate" ]; then
     echo "Creating Python virtual environment in $VENV_DIR..."
     python3 -m venv "$VENV_DIR"
